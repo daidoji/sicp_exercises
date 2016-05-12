83b8b5536dc78e77c01a347e8db58137dfde233a
Charles Lanahan <charles@revup.com>
2016-05-11 20:34:13 -0700

    Exercises up through 1.27

--- exercises_1_2_6.scm
+++ exercises_1_2_6.scm
@@ -19,15 +19,6 @@
 ;;; 9.0.1 of MIT Scheme most of these tests are far too slow to notice any
 ;;; difference in these orders of magnitude.
 
-(define (smallest-divisor n)
-    (find-divisor n 2))
-(define (find-divisor n test-divisor)
-    (cond ((> (square test-divisor) n) n)
-          ((divides? test-divisor n) test-divisor)
-          (else (find-divisor n (+ test-divisor 1)))))
-(define (divides? a b)
-    (= (remainder b a) 0))
-
 (define (prime? n)
     (= n (smallest-divisor n)))
 
@@ -35,12 +26,15 @@
     (newline)
     (display n)
     (start-prime-test n (runtime)))
+
 (define (start-prime-test n start-time)
     (if (prime? n)
         (report-prime (- (runtime) start-time))))
+
 (define (report-prime elapsed-time)
     (display " *** ")
-      (display elapsed-time))
+    (display elapsed-time)
+    elapsed-time)
 
 (define (search-for-primes a b)
   (cond ((>= a b) a)
@@ -57,21 +51,65 @@
 (search-for-primes 1000001 1000100)
 
 ;;; Exercise 1.23
-;;; b
-(define (next input)
-  (if (= (- input 2) 0) 3 (+ input 2)))
+;;; refine procedure so that it runs much faster than original one due to fact
+;;; that after we've checked 2 we no longer need to check all the evens.
 
-(define (smallest-divisor n)
-    (find-divisor n 2))
+(define (find-divisor n test-divisor) 
+  (cond ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor) 
+    (else (find-divisor n (+ test-divisor 1)))))
 
-(define (find-divisor n test-divisor)
-    (cond ((> (square test-divisor) n) n)
-          ((divides? test-divisor n) test-divisor)
-          (else (find-divisor n (next test-divisor)))))
+(search-for-primes 1001 1100)
+(search-for-primes 10001 10100)
+(search-for-primes 100001 100100)
+(search-for-primes 1000001 1000100)
 
-(define (divides? a b)
-    (= (remainder b a) 0))
+;;; Exercise 1.24
+;;; 
+;;; we modify the timed-prime-test to use the probalistic fermat method instead
+;;; of a deterministic sieve and evalute the time of some of the primes we
+;;; found in exercise 1.22
 
-(timed-prime-test 199)
-(timed-prime-test 19999)
-(timed-prime-test 199999)
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) 
+         (remainder (square (expmod base (/ exp 2) m))
+                     m))
+        (else 
+          (remainder (* base (expmod base (- exp 1) m))
+                      m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+    (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+    (else false)))
+
+(define (start-prime-test n start-time)
+    (if (fast-prime? n 10000) (report-prime (- (runtime) start-time))))
+
+(timed-prime-test 1000003)
+(timed-prime-test 1000033)
+(timed-prime-test 1000037)
+(timed-prime-test 1000039)
+(timed-prime-test 1000081)
+(timed-prime-test 1000099)
+
+;;; Excercise 1.27
+;;;
+;;; Demonstrate the Carmicheal numbers 561, 1105, 1729, 2465, 2821, and 6601
+;;; fool the fermat-test.  Then write a procedure that takes an integer n and
+;;; tests whether a^n is congruent to a mod n for all a < n and try that
+;;; function on the Carmicheal numbers
+
+(fermat-test 4)
+(fermat-test 561)
+(fermat-test 1105)
+(fermat-test 1729)
+(fermat-test 2465)
+(fermat-test 2821)
+(fermat-test 6601)
